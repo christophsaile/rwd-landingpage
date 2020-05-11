2bf477dd299ccd4ecc64b90be298baadd24c8e74
Christoph Saile <christoph.saile@googlemail.com>
2020-05-11 20:59:41 +0200

    add: counter + learnMore to nav

--- src/components/rwd-nav/rwd-nav.ts
+++ src/components/rwd-nav/rwd-nav.ts
@@ -2,6 +2,8 @@ import Component, { HTMLFragment, createRef } from '@biotope/element';
 import { template } from './template';
 import { RwdNavProps, RwdNavState, RwdNavMethods } from './interfaces';
 
+import * as ScrollMagic from "scrollmagic";
+
 export class RwdNav extends Component< RwdNavProps, RwdNavState > {
   public static componentName = 'rwd-nav';
   
@@ -9,12 +11,25 @@ export class RwdNav extends Component< RwdNavProps, RwdNavState > {
 
   private refs = {
     menuIconRef: createRef<HTMLElement>(),
+    counterRef: createRef<HTMLElement>(),
+    counterFirstItemRef: createRef<HTMLElement>(),
+    counterLastItemRef: createRef<HTMLElement>(),
+    learnMoreRef: createRef<HTMLElement>(),
+    learnMoreTextRef: createRef<HTMLElement>(),
+    learnMoreIconRef: createRef<HTMLElement>(),
   };
 
+  public allSections: NodeListOf<HTMLElement>;
+
   ready() {
+    this.allSections = document.querySelectorAll(".fullpage");
 
+    this.initMenu();
+    this.initCounter();
+  }
+
+  public initMenu = () => {
     this.refs.menuIconRef.current.addEventListener("click", this.handleMenuClick);
-
   }
 
   public handleMenuClick = () => {
@@ -24,7 +39,33 @@ export class RwdNav extends Component< RwdNavProps, RwdNavState > {
     console.log(this.state.isMenuOpen);
   }
 
+  public initCounter = () => {
+    this.setLastPageNumber();
+    this.counterScrollAnimation();
+  }
 
+  public setLastPageNumber = () => {
+    const numberOfSections: String = this.allSections.length.toString();
+		this.refs.counterLastItemRef.current.innerHTML = "0" + numberOfSections;
+  }
+
+  public counterScrollAnimation = () => {
+    const currentPage: HTMLElement = this.refs.counterFirstItemRef.current;
+		let controller = new ScrollMagic.Controller();
+
+		this.allSections.forEach((element, index) => {
+			let scene = new ScrollMagic.Scene({
+				triggerElement: element,
+				duration: "100%",
+				triggerHook: 0.5
+			})
+				.addTo(controller)
+				.on("enter", function() {
+					currentPage.innerHTML = "0" + (index + 1);
+					currentPage.setAttribute("href", '#'+element.id);
+				});
+		});
+  }
 
   protected readonly defaultProps: RwdNavProps = {};
 
