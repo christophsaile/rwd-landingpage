6a8f63f0c4de54a53105f9cb5fb6b6e73d984e97
Christoph Saile <christoph.saile@googlemail.com>
2020-06-29 22:07:52 +0200

    add: flower animation

--- src/components/rwd-production/rwd-production.ts
+++ src/components/rwd-production/rwd-production.ts
@@ -22,21 +22,50 @@ export class RwdProduction extends Component<
 	public methods: RwdProductionMethods = {};
 
 	ready() {
-    this.initScrollAnimation();
-  }
+		this.initScrollAnimation();
+	}
 
-  private initScrollAnimation  = () => {
-    const socialSection = this.shadowRoot.querySelector('.social__links');
-    const socialTrigger = this.shadowRoot.querySelector('.social__scrollTrigger');
+	private initScrollAnimation = () => {
+		const socialSection = this.shadowRoot.querySelector(".social__links");
+		const socialTrigger = this.shadowRoot.querySelector(
+			".social__scrollTrigger"
+		);
+		const flowers = this.shadowRoot.querySelectorAll(
+			".production__flowers svg"
+		);
 
-    let controller = new ScrollMagic.Controller();
-    let scene = new ScrollMagic.Scene({
-      triggerElement: socialTrigger,
-      triggerHook: "onEnter",
-    })
-    .setClassToggle(socialSection, 'animate__fadeInUp')
-		.addTo(controller)
-  }
+		console.log(flowers);
+
+		let controller = new ScrollMagic.Controller();
+		let socialAnimation = new ScrollMagic.Scene({
+			triggerElement: socialTrigger,
+			triggerHook: "onEnter"
+		})
+			.setClassToggle(socialSection, "animate__fadeInUp")
+			.addTo(controller);
+
+		let flowersAnimation = new ScrollMagic.Scene({
+			triggerElement: "#production",
+			triggerHook: "onEnter"
+		})
+			.on("enter", () => {
+				flowers.forEach(item => {
+					if (item.classList.contains("production__flowers--left")) {
+						console.log(item.classList);
+						item.classList.add("animate__fadeInLeft");
+						item.addEventListener("animationend", function() {
+							item.classList.remove("animate__fadeInLeft");
+						});
+					} else {
+						item.classList.add("animate__fadeInRight");
+						item.addEventListener("animationend", function() {
+							item.classList.remove("animate__fadeInRight");
+						});
+					}
+				});
+			})
+			.addTo(controller);
+	};
 
 	public render(): HTMLFragment {
 		return template({ ...this.props, ...this.state, ...this.methods });
