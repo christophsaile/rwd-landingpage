93260ef5aa6ba6cc054028a1d756ecf6b70d8992
Christoph Saile <christoph.saile@googlemail.com>
2020-06-29 20:45:58 +0200

    add: scroll animation

--- src/components/rwd-production/rwd-production.ts
+++ src/components/rwd-production/rwd-production.ts
@@ -1,21 +1,46 @@
-import Component, { HTMLFragment } from '@biotope/element';
-import { template } from './template';
-import { RwdProductionProps, RwdProductionState, RwdProductionMethods } from './interfaces';
+import Component, { HTMLFragment } from "@biotope/element";
+import { template } from "./template";
+import {
+	RwdProductionProps,
+	RwdProductionState,
+	RwdProductionMethods
+} from "./interfaces";
+import * as ScrollMagic from "scrollmagic";
+import { ADDRGETNETWORKPARAMS } from "dns";
+export class RwdProduction extends Component<
+	RwdProductionProps,
+	RwdProductionState
+> {
+	public static componentName = "rwd-production";
 
-export class RwdProduction extends Component< RwdProductionProps, RwdProductionState > {
-  public static componentName = 'rwd-production';
-  
-  public static attributes = [];
+	public static attributes = [];
 
-  protected readonly defaultProps: RwdProductionProps = {};
-   
-  protected readonly defaultState: RwdProductionState = {};
+	protected readonly defaultProps: RwdProductionProps = {};
 
-  public methods: RwdProductionMethods = {};
+	protected readonly defaultState: RwdProductionState = {};
 
-  public render(): HTMLFragment {
-    return template( { ...this.props, ...this.state, ...this.methods });
+	public methods: RwdProductionMethods = {};
+
+	ready() {
+    this.initScrollAnimation();
   }
+
+  private initScrollAnimation  = () => {
+    const socialSection = this.shadowRoot.querySelector('.social__links');
+    const socialTrigger = this.shadowRoot.querySelector('.social__scrollTrigger');
+
+    let controller = new ScrollMagic.Controller();
+    let scene = new ScrollMagic.Scene({
+      triggerElement: socialTrigger,
+      triggerHook: "onEnter",
+    })
+    .setClassToggle(socialSection, 'animate__fadeInUp')
+		.addTo(controller)
+  }
+
+	public render(): HTMLFragment {
+		return template({ ...this.props, ...this.state, ...this.methods });
+	}
 }
 
 RwdProduction.register();
